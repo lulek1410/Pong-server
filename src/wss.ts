e85a49fbbca6b89923ac3a8b8a3f69b2cd26746b
lulek1410 <jan.szewczynski@gmail.com>
2024-06-19 20:16:22 +0200

    (feat): Add leave room functionality

--- src/wss.ts
+++ src/wss.ts
@@ -14,7 +14,7 @@ interface JoinMessage {
 type Message = JoinMessage | ConnectionMessage;
 
 const maxClients = 2;
-let rooms: { [key: string]: [WebSocket] } = {};
+let rooms: { [key: string]: WebSocket[] } = {};
 
 const sendInformation = (ws: WebSocket) => {
   let obj;
@@ -80,6 +80,13 @@ export const configureWss = (server: Server) => {
       sendInformation(ws);
     };
 
-    const leave = () => {};
+    const leave = () => {
+      const room = ws["room"];
+      rooms[room] = rooms[room].filter((client) => client !== ws);
+      ws["room"] = undefined;
+      if (rooms[room].length === 0) {
+        delete rooms[room];
+      }
+    };
   });
 };
